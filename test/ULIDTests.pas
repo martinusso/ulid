8ecb2c4e371bc2ed1cd66155779f3e0ff8a1e876
Breno Martinusso <martinusso@gmail.com>
2017-01-06 10:25:36 -0200

    test lexicographically sortable

--- test/ULIDTests.pas
+++ test/ULIDTests.pas
@@ -10,11 +10,16 @@ type
   TULIDTests = class(TTestCase)
   published
     procedure TestCreateULID;
+    procedure TestSortable;
     procedure TestEncodeTime;
   end;
 
 implementation
 
+uses
+  Classes,
+  SysUtils;
+  
 { TULIDTests }
 
 procedure TULIDTests.TestCreateULID;
@@ -29,8 +34,35 @@ procedure TULIDTests.TestEncodeTime;
 var
   Got: string;
 begin
-  Got := EncodeTime(1469918176385);
+  Got := ULID.EncodeTime(1469918176385);
   CheckEquals('01ARYZ6S41', Got);
+end;
+
+procedure TULIDTests.TestSortable;
+var
+  I: Integer;
+  Ulids, SortedUlids: TStringList;
+begin
+  Ulids := nil;
+  SortedUlids := nil;
+  try
+    Ulids := TStringList.Create;
+    for I := 0 to 10 do
+    begin
+      Ulids.Add(CreateULID);
+      Sleep(42);
+    end;
+
+    SortedUlids := TStringList.Create;
+    SortedUlids.Text := Ulids.Text;
+    SortedUlids.Sort;
+
+    for I := 0 to Ulids.Count - 1 do
+      CheckEquals(SortedUlids[I], Ulids[I]);
+  finally
+    Ulids.Free;
+    SortedUlids.Free;
+  end;
 end;
 
 initialization
